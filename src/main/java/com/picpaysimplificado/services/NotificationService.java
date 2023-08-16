b27884906551dfb69ef00169d52b9707326948c3
Thais Regina Mendes Batista <thais.batista@maximatech.com.br>
2023-08-16 17:43:01 -0300

    feat: create Notification service

---
+++ src/main/java/com/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,31 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.NotificationDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+
+    Logger logger = LoggerFactory.getLogger(NotificationService.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+        if (!notificationResponse.getStatusCode().equals(HttpStatus.OK)) {
+            logger.info("erro ao enviar a notificação");
+            throw new Exception("Serviço de Notificação está fora do ar");
+        }
+    }
+}
