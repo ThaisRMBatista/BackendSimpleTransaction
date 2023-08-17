8082c9d6ecc87c3636ab2098b6ceba6f6286dd6b
Thais Regina Mendes Batista <thais.batista@maximatech.com.br>
2023-08-17 16:46:14 -0300

    fix: implement jsonSerialize on NotificationDTO

--- src/main/java/com/transferenciasimplificada/dtos/NotificationDTO.java
+++ src/main/java/com/transferenciasimplificada/dtos/NotificationDTO.java
@@ -1,5 +1,6 @@
 package com.transferenciasimplificada.dtos;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -7,6 +8,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
+@JsonSerialize
 public final class NotificationDTO {
     private final String email;
     private final String message;
